9cdd74cdafbdd3c1d9a9e817f71b568976fdd701
yewon97 <dpdnjs9936@gmail.com>
2022-01-11 16:13:12 +0900

    useState 사용방법

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+// react에 있는 내장 함수를 가져다가 쓰겠습니다!
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useState('남자 코트 추천'); // -> [a, b] 형식으로 출력
+  // a -> '남자 코트 추천'
+  // b -> '남자 코트 추천' state를 정정해주는 함수
+
   let posts = '강남 고기 맛집'; // 변수에 데이터 저장하고
   return (
     <div className="App">
