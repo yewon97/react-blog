d2ac9af61ad5be4104cf17dd72f60c591c55f759
yewon97 <dpdnjs9936@gmail.com>
2022-01-11 17:02:05 +0900

    버튼 누르면 '여자 코트 변경'으로 바꾸기

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
+  function 제목바꾸기() {
+    글제목변경(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  }
+  
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={제목바꾸기}>버튼</button>
       <div className="list">
         <h4>{글제목[0]} <span onClick={ () => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
