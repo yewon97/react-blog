32aa93b964e44f249d769b046096eba439d7631b
yewon97 <dpdnjs9936@gmail.com>
2022-01-11 16:09:50 +0900

    글 목록 생성해봄

--- src/App.js
+++ src/App.js
@@ -3,27 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let posts = '강남 고기 맛집'; // 서버에서 가져온 데이터
-
-  let st_1 = { color: 'blue', fontStyle: 'italic' };
-  function 함수(x) {
-    return x;
-  }
-
+  let posts = '강남 고기 맛집'; // 변수에 데이터 저장하고
   return (
-    {
-      /* HTML처럼 생겼지만 JSX라는 문법 */
-    },
-    (
-      <div className="App">
-        <div className="black-nav">
-          <div style={st_1}>개발 Blog</div>
-        </div>
-        <h4>{함수(100)}</h4>
-        <h4>{posts}</h4>
-        <img src={logo} alt={logo} />
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
       </div>
-    )
+      <div className='list'>
+        <h4>{posts}</h4> {/* 데이터 바인딩 했음 */}
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+    </div>
   );
 }
 
