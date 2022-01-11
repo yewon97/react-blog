9d02faf6707d795153f3000e1cdff69ea5f2792b
yewon97 <dpdnjs9936@gmail.com>
2022-01-11 16:05:52 +0900

    react 연습하기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = '강남 고기 맛집'; // 서버에서 가져온 데이터
+
+  let st_1 = { color: 'blue', fontStyle: 'italic' };
+  function 함수(x) {
+    return x;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {
+      /* HTML처럼 생겼지만 JSX라는 문법 */
+    },
+    (
+      <div className="App">
+        <div className="black-nav">
+          <div style={st_1}>개발 Blog</div>
+        </div>
+        <h4>{함수(100)}</h4>
+        <h4>{posts}</h4>
+        <img src={logo} alt={logo} />
+      </div>
+    )
   );
 }
 
